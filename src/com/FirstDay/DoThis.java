22e1e11ebce5361f8326d955f786a250a5fb9eb3
Roman <lenin.jun@gmail.com>
2015-06-10 11:36:07 +0300

    Start Project

---
+++ src/com/FirstDay/DoThis.java
@@ -0,0 +1,8 @@
+package com.FirstDay;
+
+/**
+ * Created by Вевдюк on 10.06.2015.
+ */
+public class DoThis {
+    
+}
---
+++ src/com/FirstDay/Main.java
@@ -0,0 +1,10 @@
+package com.FirstDay;
+
+/**
+ * Created by Вевдюк on 10.06.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
