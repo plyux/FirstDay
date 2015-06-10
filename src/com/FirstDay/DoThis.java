bde8dbcdeabbceca850213d578fb33520c49677f
Roman <lenin.jun@gmail.com>
2015-06-10 12:15:00 +0300

    First result

--- src/com/FirstDay/DoThis.java
+++ src/com/FirstDay/DoThis.java
@@ -1,8 +1,38 @@
 package com.FirstDay;
 
+import java.util.ArrayList;
+import java.util.*;
+
 /**
  * Created by Вевдюк on 10.06.2015.
  */
 public class DoThis {
-    
+
+    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public static Character Find( String inputString ){
+        Character required=' ';
+        char[] stringToCharArray = inputString.toCharArray();
+        Map<Character , Integer > checkSum = new HashMap<Character, Integer>();
+        for(int i = 0; i < inputString.length(); i++){
+            char currentCharacter = stringToCharArray[i];
+            if(checkSum.containsKey(currentCharacter)){
+                checkSum.put(currentCharacter ,checkSum.get(currentCharacter)+1);
+               // System.out.println("Key: \'" + currentCharacter + "\' Value: " + checkSum.get(currentCharacter));
+            }
+            else{
+                checkSum.put(currentCharacter, 1);
+            }
+        }
+        required = getKeyByValue(checkSum, 1);
+        return required;
+}
+
 }
--- src/com/FirstDay/Main.java
+++ src/com/FirstDay/Main.java
@@ -1,10 +1,17 @@
 package com.FirstDay;
 
+import java.util.Scanner;
+
 /**
  * Created by Вевдюк on 10.06.2015.
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Input you line: ");
+        String inputLine = " ";
+        Scanner scanner = new Scanner(System.in);
+        inputLine = scanner.nextLine();
+        char requiredChar = DoThis.Find(inputLine);
+        System.out.println("Required symbol is: " + requiredChar);
     }
 }
